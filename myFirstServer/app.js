1246f17086b3d3040367077763626bd534571e2d
Emil <emil.delrosso@elev.ga.ntig.se>
2020-09-10 14:26:42 +0200

    uppgift 1 webbserver

--- myFirstServer/app.js
+++ myFirstServer/app.js
@@ -1,7 +1,14 @@
-const labb = require('./labb')
+// const labb = require('./labb')
 
-let sum = labb.add(5, 2)
-let dif = labb.sub(5, 2)
-let prod = labb.multi(5, 2)
+// let sum = labb.add(5, 2)
+// let dif = labb.sub(5, 2)
+// let prod = labb.multi(5, 2)
 
-console.log("Summan är: " + sum + " Differensen är: " + dif + " Produkten är: " + prod)
+// console.log("Summan är: " + sum + " Differensen är: " + dif + " Produkten är: " + prod)
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port port!`))
